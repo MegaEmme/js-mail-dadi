2a94d6a56ec4d555a1efbf30574755c8642e93c3
MegaEmme <mikmusco@gmail.com>
2025-02-27 15:09:09 +0100

    feat: modified pseudo-code

--- mail/script.js
+++ mail/script.js
@@ -1,13 +1,15 @@
 console.log("Mike's Mail list")
 
 /*  - creo una lista partecipanti
+    - la stampo a schermo
     - chiedo all'utente di inserire la mail
-    cerco nella lista se c'è la mail dell'utente 
-    **se** è presente
-        - stampo "presente"
-    ** altrimenti** 
-        - stampo "non presente" 
-*/
+    - **per** ogni nome nell'elenco
+        - cerco nella lista se c'è la mail dell'utente 
+        - **se** è presente
+            - stampo "presente"
+            - chiudo il ciclo
+    - **se** non è presente
+        - stampo "non presente" */
 
 const mailList = ['pippo@gmail.com','pluto@gmail.com','paperino@gmail.com','topolino@gmail.com','minnie@gmail.com','clarabella@gmail.com', 'pietro@gmail.com','qui@gmail.com','quo@gmail.com','qua@gmail.com','paperone@gmail.com','nonnapapera@gmail.com','etabeta@gmail.com','paperina@gmail.com'];
 
@@ -21,6 +23,7 @@ for (i=0; i<mailList.length;i++){
         break;
     }     
 }
- if (userMail !== mailList[i]){
+
+if (userMail !== mailList[i]){
     console.log("Spiacente, la tua mail non è presenta nell'elenco");
- }
+}
