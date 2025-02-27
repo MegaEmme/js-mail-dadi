7961c154e3d5d19aa550a10531a29ce46c4d63af
MegaEmme <mikmusco@gmail.com>
2025-02-27 15:02:13 +0100

    solved es.1

--- mail/script.js
+++ mail/script.js
@@ -1,1 +1,26 @@
 console.log("Mike's Mail list")
+
+/*  - creo una lista partecipanti
+    - chiedo all'utente di inserire la mail
+    cerco nella lista se c'è la mail dell'utente 
+    **se** è presente
+        - stampo "presente"
+    ** altrimenti** 
+        - stampo "non presente" 
+*/
+
+const mailList = ['pippo@gmail.com','pluto@gmail.com','paperino@gmail.com','topolino@gmail.com','minnie@gmail.com','clarabella@gmail.com', 'pietro@gmail.com','qui@gmail.com','quo@gmail.com','qua@gmail.com','paperone@gmail.com','nonnapapera@gmail.com','etabeta@gmail.com','paperina@gmail.com'];
+
+console.log(mailList); 
+
+let userMail = prompt("Inserisci la tua mail");
+
+for (i=0; i<mailList.length;i++){
+    if (userMail === mailList[i]) {
+        console.log('Ho trovato la tua mail');
+        break;
+    }     
+}
+ if (userMail !== mailList[i]){
+    console.log("Spiacente, la tua mail non è presenta nell'elenco");
+ }
