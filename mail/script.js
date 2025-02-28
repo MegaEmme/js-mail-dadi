c33cfece6bc540e1e65e011a941a213e86c73fb5
MegaEmme <mikmusco@gmail.com>
2025-02-28 10:01:44 +0100

    feat: modified es.1

--- mail/script.js
+++ mail/script.js
@@ -1,6 +1,7 @@
 console.log("Mike's Mail list")
 
-/*  - creo una lista partecipanti
+/* 
+    - creo una lista partecipanti
     - la stampo a schermo
     - chiedo all'utente di inserire la mail
     - **per** ogni nome nell'elenco
@@ -9,7 +10,8 @@ console.log("Mike's Mail list")
             - stampo "presente"
             - chiudo il ciclo
     - **se** non è presente
-        - stampo "non presente" */
+        - stampo "non presente" 
+*/
 
 const mailList = ['pippo@gmail.com','pluto@gmail.com','paperino@gmail.com','topolino@gmail.com','minnie@gmail.com','clarabella@gmail.com', 'pietro@gmail.com','qui@gmail.com','quo@gmail.com','qua@gmail.com','paperone@gmail.com','nonnapapera@gmail.com','etabeta@gmail.com','paperina@gmail.com'];
 
@@ -17,7 +19,7 @@ console.log(mailList);
 
 let userMail = prompt("Inserisci la tua mail");
 
-for (i=0; i<mailList.length;i++){
+for (i=0; i<mailList.length; i++){
     if (userMail === mailList[i]) {
         console.log('Ho trovato la tua mail');
         break;
